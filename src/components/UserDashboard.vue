0758b48c20c95e4f1277bd5cd1d47e3200695661
Hillary Okporka <hillariousice@gmail.com>
2025-06-05 22:35:27 +0100

    Update UserDashboard.vue

--- src/components/UserDashboard.vue
+++ src/components/UserDashboard.vue
@@ -60,7 +60,8 @@ const checkAuth = () => {
     if (user) {
       isAuthenticated.value = true;
       userProfile.value = {
-        name: user.firstName || "User",
+        name: user?.email
+          || "User",
         profilePicture: user.photoURL || "https://via.placeholder.com/150"
       };
     } else {
