1b4a8a2894c89c261bdfbcd3a9983ed27038f3c1
Hillary Okporka <hillariousice@gmail.com>
2025-06-06 17:13:35 +0100

    Update AddJobView.vue

--- src/views/jobs/AddJobView.vue
+++ src/views/jobs/AddJobView.vue
@@ -82,7 +82,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <section class="bg-green-50">
+  <section class="bg-white">
     <div class="container m-auto max-w-2xl py-24">
       <div class="bg-white px-4 sm:px-6 py-6 sm:py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
         <form v-if="isAuthenticated" @submit.prevent="handleSubmit">
