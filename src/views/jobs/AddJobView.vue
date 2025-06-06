400ee12fd6365bfca03225322a6ec8368c7982da
Hillary Okporka <hillariousice@gmail.com>
2025-06-06 16:57:02 +0100

    Update AddJobView.vue

--- src/views/jobs/AddJobView.vue
+++ src/views/jobs/AddJobView.vue
@@ -163,7 +163,7 @@ onMounted(() => {
           </div>
 
           <div>
-            <button class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline" type="submit">Add Job</button>
+            <button class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg w-full focus:outline-none focus:shadow-outline" type="submit">Add Job</button>
           </div>
         </form>
       </div>
